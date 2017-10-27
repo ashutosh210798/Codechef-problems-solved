cde0c55e055f487da069f687ea46d5d5259a028e
Ashutosh Maheshwari <ashutoshmaheshwari210798@gmail.com>
2017-10-28 02:42:29 +0530

    Create MUFFINS3.c

---
+++ MUFFINS3.c
@@ -0,0 +1,18 @@
+//This comes under easy category
+
+#include <cstdio>
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	int tc,a;
+	cin >>tc;
+	while(tc--)
+	{
+		cin>>a;
+		cout<<(a/2)+1<<endl;
+	}
+}
